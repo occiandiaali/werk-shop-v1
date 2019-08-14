bbbefefdfc3968d0878f4e6fc0cc53e35babe3fb
ocean <ocean@localhost.localdomain>
2019-08-14 17:16:20 +0100

    add routes to categories

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,8 @@ import Home from './Home';
 import About from './About';
 import SignIn from './SignInForm';
 import SignUp from './SignUpForm';
+import Category from './Category';
+import HirerInfo from './HirerInfo';
 
 const Main = () => {
     return(
@@ -11,8 +13,14 @@ const Main = () => {
             <Switch>
               <Route path="/" exact component={Home} />
               <Route path="/about" component={About} />
+              <Route path="/hirer-info" component={HirerInfo}/>
               <Route path="/signin" component={SignIn} />
               <Route path="/signup" component={SignUp} />
+              <Route path="/automotive" component={Category}/>
+              <Route path="/domestic" component={Category} />
+              <Route path="/electrical" component={Category} />
+              <Route path="/artcraft" component={Category} />
+              <Route path="/academic" component={Category} />
             </Switch>
         </main>
     );
